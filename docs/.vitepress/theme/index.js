73aa95e75f2b57ff80566e9bb87e221a100a1c5b
diarboy <bigdickdumb@gmail.com>
2025-03-04 09:08:39 +0700

    test2 88s

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,7 +1,7 @@
 import DefaultTheme from 'vitepress/theme';
 // import PinterestEmbed from './components/PinterestEmbed.vue';
-// import GalleryMasonry from './components/GalleryMasonry.vue';
-// import GalleryCarousel from './components/GalleryCarousel.vue';
+import GalleryMasonry from './components/GalleryMasonry.vue';
+import GalleryCarousel from './components/GalleryCarousel.vue';
 // import pdfmake from './components/pdfmake.vue';
 import FAQ from './components/FAQ.vue';
 
@@ -10,8 +10,8 @@ export default {
   enhanceApp({ app }) {
     app.component('FAQ', FAQ);
   //   app.component('PinterestEmbed', PinterestEmbed);
-  //   app.component('GalleryMasonry', GalleryMasonry);
-  //   app.component('GalleryCarousel', GalleryCarousel);
+    app.component('GalleryMasonry', GalleryMasonry);
+    app.component('GalleryCarousel', GalleryCarousel);
   //   app.component('pdfmake', pdfmake);
   },
 };
