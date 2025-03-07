393856d74a1bc6c0ee209e1d41236ee535ab5826
diarboy <bigdickdumb@gmail.com>
2025-03-07 19:26:52 +0700

    failed

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -18,28 +18,5 @@ export default {
     app.component('GalleryCarousel', GalleryCarousel);
     app.component('pdfmake', pdfmake);
     app.component('Blog', Blog);
-    
-    if (typeof window !== 'undefined') {
-      let touchStartX = 0;
-      let touchEndX = 0;
-
-      document.addEventListener("touchstart", (e) => {
-        touchStartX = e.touches[0].clientX;
-      });
-
-      document.addEventListener("touchmove", (e) => {
-        touchEndX = e.touches[0].clientX;
-      });
-
-      document.addEventListener("touchend", () => {
-        if (touchStartX - touchEndX > 50) {
-          // Swipe kiri (tutup sidebar)
-          document.body.classList.remove("show-sidebar");
-        } else if (touchEndX - touchStartX > 50) {
-          // Swipe kanan (buka sidebar)
-          document.body.classList.add("show-sidebar");
-        }
-      });
-    }
   },
 };
