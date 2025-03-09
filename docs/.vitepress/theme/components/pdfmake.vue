b060d6eb4f298d12a805141f721777c28de244c0
Ardi Syah <bigdickdumb@gmail.com>
2025-03-09 22:56:12 +0700

    Update pdfmake.vue

--- docs/.vitepress/theme/components/pdfmake.vue
+++ docs/.vitepress/theme/components/pdfmake.vue
@@ -81,11 +81,11 @@ const downloadDOCX = async () => {
 <h2 class="subtitle" style="text-align: center;">This Playground is <br>a Work in Progress</h2>
   <div class="playground-container">
     <div class="editor">
-      <h3 class="title">Markdown Editor</h3>
+      <h1 class="title">Markdown Editor</h1>
       <textarea v-model="markdownText" placeholder="Tulis Markdown di sini..."></textarea>
     </div>
     <div class="preview">
-      <h3 class="title">Preview HTML</h3>
+      <h1 class="title">Preview HTML</h1>
       <div v-html="htmlOutput" class="output"></div>
       <div class="button-container">
         <a class="VPButton" @click="copyToClipboard">
@@ -107,7 +107,6 @@ const downloadDOCX = async () => {
   display: flex;
   flex-direction: column;
   gap: 20px;
-  margin-top: 5px;
 }
 
 .editor, .preview {
@@ -119,8 +118,10 @@ textarea {
   height: 300px;
   padding: 10px;
   border: 1px solid #ccc;
-  border-radius: 5px;
+  border-radius: 10px;
   font-family: monospace;
+  background: transparent;
+  margin-top: 5px;
 }
 
 .output {
@@ -128,7 +129,7 @@ textarea {
   min-height: 300px;
   padding: 10px;
   background: transparent;
-  border-radius: 5px;
+  border-radius: 10px;
   overflow: auto;
   word-wrap: break-word;
   color: var(--vp-c-text-1);
